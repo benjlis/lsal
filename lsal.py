709a1328dd07b4ace588e227a6050562bab3182b
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-03-01 09:06:02 -0500

    Added csv export of loader file

--- lsal.py
+++ lsal.py
@@ -1,17 +1,23 @@
 import pathlib
 import json
+import csv
 
 data = pathlib.Path('data')
 alist = list(data.iterdir())
 alist.sort()
-for a in alist:
-    with open(a) as file:
-        doc = json.loads(file.read())
-        atup = (int(doc["id"]), doc["completed_by"]["email"],
-                doc["task"]["updated_at"], doc["task"]["data"]["url"])
-        print(atup)
-        for r in doc["result"]:
-            # print(r)
-            print(r["value"]["start"], 
-                  r["value"]["end"], 
-                  r["value"]["labels"][0])
+with open('loader.csv', 'w', newline='') as csvfile:
+    redactwriter = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+    for a in alist:
+        with open(a) as file:
+            doc = json.loads(file.read())
+            s3_address = doc["task"]["data"]["url"]
+            docid = s3_address[s3_address.rfind('/') + 1:\
+                               len(s3_address)-4]
+            lead = (int(doc["id"]), 
+                    doc["completed_by"]["email"],
+                    doc["task"]["updated_at"], docid)
+            for r in doc["result"]:
+                redaction = lead + (r["value"]["start"], 
+                                    r["value"]["end"], 
+                                    r["value"]["labels"][0])
+                redactwriter.writerow(redaction)
