7d8fa12728814ed185c8cdd267a6abb48b63bd3c
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-03-06 13:36:53 -0500

    Initial commit

---
+++ loader.sql
@@ -0,0 +1,14 @@
+-- load program
+\copy piir_eval.ground_truth_stage(label_studio_id, creator, completed, doc, start_idx, end_idx, entity_code) from 'loader.csv' csv
+-- load from stage
+insert into piir_eval.ground_truth (test_id, entity_code, 
+    start_idx, end_idx, label_studio_id, creator, completed)
+select t.test_id,
+       case when ts.entity_code = 'SSN'     then 'ssn'
+            when ts.entity_code = 'Account' then 'ban'
+            when ts.entity_code = 'Phone'   then 'phone_number'
+       end entity_code, 
+       ts.start_idx, ts.end_idx, 
+       ts.label_studio_id, ts.creator, ts.completed
+    from piir_eval.tests t join piir_eval.ground_truth_stage ts
+        on (t.doc_id = ts.doc);
